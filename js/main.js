787623803c3e80c932c5baa0520a3adf49930590
GlaDdos <kamil.kawa@gmail.com>
2017-11-30 22:34:35 +0100

    Chcnging png icons to svg, rwd changes in css

--- js/main.js
+++ js/main.js
@@ -6,5 +6,21 @@ $(document).ready(function(){
     });
   });
 
-  
+
+  $('.menu-item').on('click', function () {
+
+    if($(this).hasClass('active')) {
+      $(this).removeClass('active');
+    } else {
+      $('.menu-item').removeClass('active');
+      $(this).addClass('active');
+    }
+
+    if(this.id == 'blog') {
+      $('.sub-menu').show();
+    } else {
+      $('.sub-menu').hide();
+    }
+  });
+
 })
